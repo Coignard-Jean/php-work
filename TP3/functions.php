66d1b8e40877d6df7c6ea566f6073bffe7f6e90b
Coignard-Jean <73880086+Coignard-Jean@users.noreply.github.com>
2020-11-18 10:39:12 +0100

    php

---
+++ TP3/functions.php
@@ -0,0 +1,25 @@
+<?php
+function getHeader($startSession,$title){
+  if($startSession){
+    session_start();
+
+}
+ ?>
+ <!doctype html>
+ <html>
+    <head>
+        <meta charset="utf-8">
+        <title><?=$title?></title>
+    </head>
+  <body>
+<?php
+}
+
+function getFooter(){
+
+
+?>
+</body>
+</html>
+<?php
+}
---
+++ index.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Html page</title>
+    </head>
+    <body>
+    <li><a href="TP3/index.php">Tp3</a></li>
+    </body>
+</html>
